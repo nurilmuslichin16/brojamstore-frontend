623071d9eefe50b087377b894e8c3ac587618995
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-02-22 23:25:36 +0700

    Upload projek vuejs frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <HeaderBrojam />
+    <HeroBrojam />
+    <BannerBrojam />
+    <InstaBrojam />
+    <PartnerBrojam />
+    <FooterBrojam />
+  </div>
+</template>
+
+<script>
+import HeaderBrojam from '@/components/HeaderBrojam.vue'
+import HeroBrojam from '@/components/HeroBrojam.vue'
+import BannerBrojam from '@/components/BannerBrojam.vue'
+import InstaBrojam from '@/components/InstaBrojam.vue'
+import PartnerBrojam from '@/components/PartnerBrojam.vue'
+import FooterBrojam from '@/components/FooterBrojam.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderBrojam,
+    HeroBrojam,
+    BannerBrojam,
+    InstaBrojam,
+    PartnerBrojam,
+    FooterBrojam
+  }
+}
+</script>
---
+++ src/views/Product.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="Product">
+    <HeaderBrojam />
+
+    <!-- Breadcrumb Section Begin -->
+    <div class="breacrumb-section text-left">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text product-more">
+                        <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
+                        <span>Detail</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section Begin -->
+
+    <!-- Product Shop Section Begin -->
+    <section class="product-shop spad page-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="product-pic-zoom">
+                                <img class="product-big-img" :src="gambarUtama" alt="" />
+                            </div>
+                            <div class="product-thumbs">
+                                <carousel :dots="false" :nav="false" class="product-thumbs-track ps-slider">
+                                    <div @click="(changeImage(thumbs[0]))" :class="thumbs[0] == gambarUtama ? 'active' : '' " class="pt" data-imgbigurl="img/mickey1.jpg">
+                                        <img src="img/mickey1.jpg" alt="" />
+                                    </div>
+
+                                    <div @click="(changeImage(thumbs[1]))" :class="thumbs[1] == gambarUtama ? 'active' : '' " class="pt" data-imgbigurl="img/mickey2.jpg">
+                                        <img src="img/mickey2.jpg" alt="" />
+                                    </div>
+
+                                    <div @click="(changeImage(thumbs[2]))" :class="thumbs[2] == gambarUtama ? 'active' : '' " class="pt" data-imgbigurl="img/mickey3.jpg">
+                                        <img src="img/mickey3.jpg" alt="" />
+                                    </div>
+
+                                    <div @click="(changeImage(thumbs[3]))" :class="thumbs[3] == gambarUtama ? 'active' : '' " class="pt" data-imgbigurl="img/mickey4.jpg">
+                                        <img src="img/mickey4.jpg" alt="" />
+                                    </div>
+                                </carousel>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 text-left">
+                            <div class="product-details">
+                                <div class="pd-title">
+                                    <span>oranges</span>
+                                    <h3>Pure Pineapple</h3>
+                                </div>
+                                <div class="pd-desc">
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
+                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
+                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
+                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
+                                    </p>
+                                    <h4>$495.00</h4>
+                                </div>
+                                <div class="quantity">
+                                    <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Product Shop Section End -->
+
+    <RelatedBrojam />
+    <FooterBrojam />
+  </div>
+</template>
+
+<script>
+import HeaderBrojam from '@/components/HeaderBrojam.vue'
+import FooterBrojam from '@/components/FooterBrojam.vue'
+import RelatedBrojam from '@/components/RelatedBrojam.vue'
+import carousel from 'vue-owl-carousel'
+
+export default {
+  name: 'Product',
+  components: {
+    HeaderBrojam,
+    FooterBrojam,
+    RelatedBrojam,
+    carousel
+  },
+  data() {
+      return {
+          gambarUtama: 'img/mickey2.jpg',
+          thumbs: [
+              'img/mickey1.jpg',
+              'img/mickey2.jpg',
+              'img/mickey3.jpg',
+              'img/mickey4.jpg',
+          ]
+      }
+  },
+  methods: {
+      changeImage(imgURL){
+          this.gambarUtama = imgURL
+      }
+  }
+}
+</script>
+
+<style scoped>
+.product-thumbs .pt {
+    margin-right: 10px;
+}
+</style>
