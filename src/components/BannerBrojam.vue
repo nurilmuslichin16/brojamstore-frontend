8ec7d2d2ce2ed0647c812c3ca1b75b451c29ef1f
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-03-08 05:57:02 +0700

    Update banner

--- src/components/BannerBrojam.vue
+++ src/components/BannerBrojam.vue
@@ -3,11 +3,12 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" :items="3" :nav="false" :autoplay="true" :dots="false">
-                        <div class="product-item">
+                        
+                        <div class="product-item" v-for="item in products" v-bind:key="item.id">
                             <div class="pi-pic">
-                                <img src="img/products/product-4.jpg" alt="" />
+                                <img v-bind:src="item.galleries[0].photo" alt="" />
                                 <ul>
                                     <li class="w-icon active">
                                         <router-link to="/product">
@@ -20,92 +21,21 @@
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name">{{item.type}}</div>
                                 <router-link to="/product">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{item.name}}</h5>
                                 </router-link>
                                 <div class="product-price">
-                                    $14.00
-                                    <span>$35.00</span>
+                                    ${{item.price}}
+                                    <span>${{item.price}}</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <router-link to="/product">
-                                            <i class="icon_bag_alt"></i>
-                                        </router-link>
-                                    </li>
-                                    <li class="quick-view">
-                                        <router-link to="/product">+ Quick View</router-link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <router-link to="/product">
-                                    <h5>Guangzhou sweater</h5>
-                                </router-link>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <router-link to="/product">
-                                            <i class="icon_bag_alt"></i>
-                                        </router-link>
-                                    </li>
-                                    <li class="quick-view">
-                                        <router-link to="/product">+ Quick View</router-link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <router-link to="/product">
-                                    <h5>Pure Pineapple</h5>
-                                </router-link>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <router-link to="/product">
-                                            <i class="icon_bag_alt"></i>
-                                        </router-link>
-                                    </li>
-                                    <li class="quick-view">
-                                        <router-link to="/product">+ Quick View</router-link>
-                                    </li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <router-link to="/product">
-                                    <h5>Converse Shoes</h5>
-                                </router-link>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+
                     </carousel>
+                </div>
+                <div class="col-lg-12 mt-5">
+                    <h3>Produk terbaru belum tersedia untuk saat ini.</h3>
                 </div>
             </div>
         </div>
@@ -115,11 +45,23 @@
 
 <script>
 import carousel from 'vue-owl-carousel'
+import axios from 'axios'
 
 export default {
     name: 'BannerBrojam',
     components: {
         carousel
+    },
+    data() {
+        return {
+            products: []
+        };
+    },
+    mounted() {
+        axios
+        .get("http://127.0.0.1:8000/api/products")
+        .then(res => (this.products = res.data.data.data))
+        .catch(err => console.log(err));
     }
 }
 </script>
